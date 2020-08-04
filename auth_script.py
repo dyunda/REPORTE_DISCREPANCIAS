60d39a58cb2326defa9f81487b8270767461b691
Daniel Yunda <danielyunda@gmail.com>
2020-08-03 21:27:24 -0500

    agregado un query para live y para network

--- auth_script.py
+++ auth_script.py
@@ -13,28 +13,37 @@ headers_auth = {
 
 r_auth = requests.post(url_auth, headers=headers_auth, data=payload_auth)
 
-print(r_auth.status_code)
+print ("Auth status code: " + str(r_auth.status_code))
 Auth_reply = r_auth.json()
 # print(Auth_reply)
 # type(Auth_reply)
 
 token = Auth_reply['access_token']
-print(token)
+# print(token)
 
 GMOC_ID = "145176118"
 
 url_enodeb = 'http://10.132.59.6:25080/rest/radio/4g/enodeb/xid'
 
-params_enodeb = {
+params_enodeb_network = {
     "xid": GMOC_ID,
     "perspective":"NETWORK"
 }
+
+params_enodeb_live = {
+    "xid": GMOC_ID,
+    "perspective":"LIVE"
+}
+
 headers_enodeb = {
     "Accept": "application/json;charset=utf-8",
     "Authorization" : "Bearer " + token
 }
 
-r_enodeb = requests.get(url_enodeb, headers=headers_enodeb, params=params_enodeb)
+r_enodeb_network = requests.get(url_enodeb, headers=headers_enodeb, params=params_enodeb_network)
 
-print("Codigo respuesta enodeb: " + str(r_enodeb.status_code))
-print(r_enodeb.json())
+print("Codigo respuesta enodeb network: " + str(r_enodeb_network.status_code))
+
+r_enodeb_live = requests.get(url_enodeb, headers=headers_enodeb, params=params_enodeb_live)
+
+print("Codigo respuesta enodeb live: " + str(r_enodeb_live.status_code))
