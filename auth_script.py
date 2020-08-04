0e2fdd5075c00f8e89ba09885e92ed0d733f6308
Daniel Yunda <danielyunda@gmail.com>
2020-08-03 21:16:31 -0500

    agrego GET de enodeB

--- auth_script.py
+++ auth_script.py
@@ -1,20 +1,40 @@
 import requests
 
 url_auth = 'http://10.132.59.6:25080/auth/realms/OSS/protocol/openid-connect/token'
-payload = {
+payload_auth = {
     "username":"ossadmin",
     "password":"insightadmin",
     "grant_type":"password",
     "client_id":"JBossCore"
 }
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+headers_auth = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
 
-r = requests.post(url_auth, headers=headers, data=payload)
+r_auth = requests.post(url_auth, headers=headers_auth, data=payload_auth)
 
-print(r.status_code)
-Auth_reply = r.json()
+print(r_auth.status_code)
+Auth_reply = r_auth.json()
 # print(Auth_reply)
 # type(Auth_reply)
 
 token = Auth_reply['access_token']
 print(token)
+
+GMOC_ID = "145176118"
+
+url_enodeb = 'http://10.132.59.6:25080/rest/radio/4g/enodeb/xid'
+
+params_enodeb = {
+    "xid": GMOC_ID,
+    "perspective":"NETWORK"
+}
+headers_enodeb = {
+    "Accept": "application/json;charset=utf-8",
+    "Authorization" : "Bearer " + token
+}
+
+r_enodeb = requests.get(url_enodeb, headers=headers_enodeb, params=params_enodeb)
+
+print("Codigo respuesta enodeb: " + str(r_enodeb.status_code))
+print(r_enodeb.json())
