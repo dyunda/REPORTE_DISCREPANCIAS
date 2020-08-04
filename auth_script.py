eb2decd39e69f08c50646c33c86d7f79593d4cf7
Daniel Yunda <danielyunda@gmail.com>
2020-08-03 20:09:23 -0500

    correccion separando auth token

--- auth_script.py
+++ auth_script.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://10.132.59.6:25080/auth/realms/OSS/protocol/openid-connect/token'
+url_auth = 'http://10.132.59.6:25080/auth/realms/OSS/protocol/openid-connect/token'
 payload = {
     "username":"ossadmin",
     "password":"insightadmin",
@@ -9,7 +9,12 @@ payload = {
 }
 headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-r = requests.post(url, headers=headers, data=payload)
+r = requests.post(url_auth, headers=headers, data=payload)
 
 print(r.status_code)
-print(r.json())
+Auth_reply = r.json()
+# print(Auth_reply)
+# type(Auth_reply)
+
+token = Auth_reply['access_token']
+print(token)
