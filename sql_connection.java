7f35f347d41c577223ee4d57e65ab3ed53a3f4eb
Daniel Yunda <danielyunda@gmail.com>
2020-10-26 20:52:50 -0500

    prepare to upload to github

--- sql_connection.java
+++ sql_connection.java
@@ -9,13 +9,14 @@ class JdbcTest1 {
       Class.forName("oracle.jdbc.driver.OracleDriver"); 
 
       // Step 2: Establish the connection to the database 
-      String url = "jdbc:oracle:thin:@//10.132.55.239:1523/LATKRC"; 
+      String url = "jdbc:oracle:thin//10.132.55.239:1523/LATKRC"; 
       Connection conn = DriverManager.getConnection(url,"LAT_NETWORK_REPOSITORY","insight");  
     }
     catch (Exception e)
     {
       System.err.println("D'oh! Got an exception!"); 
       System.err.println(e.getMessage()); 
+      System.err.println(e.printStacktrace());
     } 
   } 
 } 
